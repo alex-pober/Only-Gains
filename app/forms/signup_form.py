6d6fd0ed85f8dc76bebf9ce19c4e4a8cb99378eb
Alex <poberezhskiy.a@gmail.com>
2022-08-17 05:12:10 +0000

    sign up form and workout form added

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -25,3 +25,6 @@ class SignUpForm(FlaskForm):
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    bio = StringField('bio')
+    profilePicImg = StringField('profilePicImg')
---
+++ app/forms/workout_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Workout
+
+class WorkoutForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired])
+    notes = StringField('Notes')
