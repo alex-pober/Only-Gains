25b8e97e693fe1e345129f9432689d018d5b003a
Alex <poberezhskiy.a@gmail.com>
2022-08-18 06:25:10 +0000

    workout POST API done

--- app/api/workout_routes.py
+++ app/api/workout_routes.py
@@ -12,4 +12,20 @@ def get_workout(id):
     workout = Workout.query.filter(Workout.user_id == id).all()
     return (workout.to_dict())
 
-#POST /api/workouts/
+#POST /api/workouts/:userId
+@workout_routes.route('/<id>', methods=["POST"])
+@login_required
+def new_workout():
+    data = request.json
+    form = WorkoutForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        workout = Workout(
+            user_id=data['user_id'],
+            title=form.data['title'],
+            notes=form.data['notes']
+        )
+        db.session.add(workout)
+        db.session.commit()
+        return workout.to_dict()
+    return (form.errors)
