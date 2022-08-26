77f15e57c031ea2444bc58e776d3fd8d6b171718
Alex <poberezhskiy.a@gmail.com>
2022-08-26 23:51:01 +0000

    got drawer workinggit add .

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -12,10 +12,22 @@ import IconButton from '@mui/material/IconButton';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'
 
-function User() {
+import Chip from '@mui/material/Chip';
+
+import PropTypes from 'prop-types';
+import { Global } from '@emotion/react';
+import { styled } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { grey } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+
+function User(props) {
   const [user, setUser] = useState({});
   const { userId }  = useParams();
+  const { window } = props;
+  const [open, setOpen] = React.useState(false);
   useEffect(() => {
     if (!userId) {
       return;
@@ -31,8 +43,36 @@ function User() {
     return null;
   }
 
+const drawerBleeding = 56;
+
+const Root = styled('div')(({ theme }) => ({
+  height: '100%',
+  backgroundColor:
+    theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}));
+
+
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
 
   return (
+    <>
       <Box sx={{ my: 3, mx: 2 }}>
         <Grid container alignItems="center">
           <Grid item xs>
@@ -52,7 +92,109 @@ function User() {
           {/* <IconButton color="primary" aria-label="upload picture" component="label">
             <EditIcon fontSize="large"/>
           </IconButton> */}
-    </Box>
+      </Box>
+      <Divider variant="middle"> <Button onClick={toggleDrawer(true)}>My Links</Button> </Divider>
+      <CssBaseline />
+      <Global
+        styles={{
+          '.MuiDrawer-root > .MuiPaper-root': {
+            height: `calc(50% - ${drawerBleeding}px)`,
+            overflow: 'visible',
+          },
+        }}
+      />
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <StyledBox
+          sx={{
+            position: 'absolute',
+            top: -drawerBleeding,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            visibility: 'visible',
+            right: 0,
+            left: 0,
+          }}
+        >
+          <Puller />
+          <Typography sx={{ p: 2, color: 'text.secondary' }}>51 results</Typography>
+        </StyledBox>
+        <StyledBox
+          sx={{
+            px: 2,
+            pb: 2,
+            height: '100%',
+            overflow: 'auto',
+          }}
+        >
+          <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+                    <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+        </StyledBox>
+      </SwipeableDrawer>
+    </>
   );
 }
 export default User;
