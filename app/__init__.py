60cc2dd0a98db82ad609056ad364e061d1600f97
Alex <poberezhskiy.a@gmail.com>
2022-11-18 02:28:33 +0000

    changed https request secure logic

--- app/__init__.py
+++ app/__init__.py
@@ -51,10 +51,18 @@ CORS(app)
 # Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
 # Well.........
+# @app.before_request
+# def https_redirect():
+#     if os.environ.get('FLASK_ENV') == 'production':
+#         if request.headers.get('X-Forwarded-Proto') == 'http':
+#             url = request.url.replace('http://', 'https://', 1)
+#             code = 301
+#             return redirect(url, code=code)
+
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
+        if not request.is_secure:
             url = request.url.replace('http://', 'https://', 1)
             code = 301
             return redirect(url, code=code)
