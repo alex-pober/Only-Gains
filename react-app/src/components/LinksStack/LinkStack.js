76592194851e48db39f334690cd591b5ccc86f6a
Alex <poberezhskiy.a@gmail.com>
2023-03-04 01:25:57 +0000

    made links acctually clickable"

--- react-app/src/components/LinksStack/LinkStack.js
+++ react-app/src/components/LinksStack/LinkStack.js
@@ -18,9 +18,15 @@ export default function LinkStack({ data }) {
         Object.values(data).map((value, index) => {
           const regex = /\/\/([^,\s]+\.[^,\s]+?)(?=\/|,|\s|$|\?|#)/g;
           const mached = value?.link.match(regex)
-          console.log(value)
+
           return (
-            <Box key={index} className={"box" + (open ? ' box_expanded' : '')}>
+            <Box key={index} className={"box" + (open ? ' box_expanded' : '')}
+              component='a'
+              sx={{textDecoration: 'none', color: 'white'}}
+              rel="noopener noreferrer"
+              href={value?.link}
+              target="_blank"
+            >
               <Avatar alt={value?.title} src={`https://icon.horse/icon/${mached}`} sx={{ position: 'inherit', width: 24, height: 24, mx: 1 }} />
               <Typography sx={{ fontSize: '1rem' }} align='center' variant="h6" my={0.5}>{value?.title}</Typography>
             </Box>
@@ -37,7 +43,7 @@ export default function LinkStack({ data }) {
 
       {!open &&
         <>
-          <div style={{cursor: 'pointer', zIndex: '12', position: 'absolute', width: '100%', height: '92px', left: '0', right: '0', marginLeft: 'auto', marginRight: 'auto', bottom: '0px', flexDirection: "column-reverse", display: 'flex' }}
+          <div style={{cursor: 'pointer', zIndex: '12', position: 'absolute', width: '100%', height: '82px', left: '0', right: '0', marginLeft: 'auto', marginRight: 'auto', bottom: '0px', flexDirection: "column-reverse", display: 'flex' }}
             onClick={() => { setOpen(!open) }}
           >
             <Typography variant="h6" align='right'
@@ -54,7 +60,7 @@ export default function LinkStack({ data }) {
               {data.length} more links
             </Typography>
           </div>
-          <div style={{ backgroundColor: '#121212', zIndex: '-1', position: 'absolute', width: '100%', height: '82px', left: '0', right: '0', marginLeft: 'auto', marginRight: 'auto', bottom: '0px', flexDirection: "column-reverse", display: 'flex' }}
+          <div style={{ backgroundColor: '#121212', zIndex: '-1', position: 'absolute', width: '100%', height: '92px', left: '0', right: '0', marginLeft: 'auto', marginRight: 'auto', bottom: '0px', flexDirection: "column-reverse", display: 'flex' }}
           >
 
           </div>
